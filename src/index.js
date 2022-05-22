8e5510009df8d10b27198a89d5a41fba2175b833
Anton <anton124990qw@mail.ru>
2022-05-22 23:06:29 +0300

    create store object

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
-import { addPost, updatePost, subscribe } from './redux/state';
-import state from './redux/state';
+import store from './redux/state';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,17 +11,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 let reRender = (state) => {
 	root.render(
 		<React.StrictMode>
-			<App state={state} addPost={ addPost } updatePost={ updatePost }/>
+			<App state={state} addPost={ store.addPost.bind(store) } updatePost={ store.updatePost.bind(store) }/>
 		</React.StrictMode>
 	);
 }
 
-reRender(state);
+reRender(store.getState());
 
 
 
 
-subscribe(reRender);
+store.subscribe(reRender);
  
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,75 +1,77 @@
-let reRender = () => {
-   console.log("Success");
-}
-
-
-
-let state = {
-    profilePage: {
-        postData: [
-            {id: 1, text: "Wassup guys :)", likes: 2},
-            {id: 2, text: "Hey buddy :)", likes: 4},
-            {id: 3, text: "How are you?", likes: 10},
-            {id: 4, text: "I'm normal, and you?", likes: 22},
-            {id: 5, text: "Awesome :)", likes: 3}
-        ],
-        newPostData: ''
+let store = {
+    _state: {
+        profilePage: {
+            postData: [
+                {id: 1, text: "Wassup guys :)", likes: 2},
+                {id: 2, text: "Hey buddy :)", likes: 4},
+                {id: 3, text: "How are you?", likes: 10},
+                {id: 4, text: "I'm normal, and you?", likes: 22},
+                {id: 5, text: "Awesome :)", likes: 3}
+            ],
+            newPostData: ''
+        },
+    
+        messagesPage: {
+            dialogsData: [
+                {id: 1, name: "Sergey", surname: "Misevich", message: "Когда в Гродно? Ты обещал что бухнем :)" },
+                {id: 2, name: "Alexandr", surname: "Onufrik", message: "Привет Антоха. Как работа?)" },
+                {id: 3, name: "Igor", surname: "Jvirblya", message: "Соскучился, го бухать Тоха xD" },
+                {id: 4, name: "Vadim", surname: "Kalechits", message: "Как там в бассейне?)" },
+                {id: 5, name: "Yana", surname: "Prusevich", message: "Удачи работать в школе дальше) Я уволилась)" },
+                {id: 6, name: "Julia", surname: "Yurchenko", message: "Приезжай в Гродно, посидим выпьем)" },
+                {id: 7, name: "Gena", surname: "Tolochko", message: "Подмени меня в субботу, хорошо?" },
+                {id: 8, name: "Anjela", surname: "Turevich", message: "Срочно ко мне в кабинет!!!!!1111!" }
+            ]
+        },
+    
+        friendsPage: {
+            friendsData: [
+                {name: 'Sergey', surname: 'Misevich'},
+                {name: 'Alexander', surname: 'Onufrik'},
+                {name: 'Julia', surname: 'Yurchenko'},
+                {name: 'Sergey', surname: 'Potapov'},
+                {name: 'Andrey', surname: 'Marchukevich'},
+                {name: 'Sergey', surname: 'Shylobod'},
+                {name: 'Vadim', surname: 'Kalechits'},
+                {name: 'Andrey', surname: 'Tolkach'},
+                {name: 'Artem', surname: 'Prokopovich'},
+                {name: 'Nikita', surname: 'Lipskiy'}
+            ]
+        }
     },
 
-    messagesPage: {
-        dialogsData: [
-            {id: 1, name: "Sergey", surname: "Misevich", message: "Когда в Гродно? Ты обещал что бухнем :)" },
-            {id: 2, name: "Alexandr", surname: "Onufrik", message: "Привет Антоха. Как работа?)" },
-            {id: 3, name: "Igor", surname: "Jvirblya", message: "Соскучился, го бухать Тоха xD" },
-            {id: 4, name: "Vadim", surname: "Kalechits", message: "Как там в бассейне?)" },
-            {id: 5, name: "Yana", surname: "Prusevich", message: "Удачи работать в школе дальше) Я уволилась)" },
-            {id: 6, name: "Julia", surname: "Yurchenko", message: "Приезжай в Гродно, посидим выпьем)" },
-            {id: 7, name: "Gena", surname: "Tolochko", message: "Подмени меня в субботу, хорошо?" },
-            {id: 8, name: "Anjela", surname: "Turevich", message: "Срочно ко мне в кабинет!!!!!1111!" }
-        ]
+    getState() {
+        return this._state;
     },
 
-    friendsPage: {
-        friendsData: [
-            {name: 'Sergey', surname: 'Misevich'},
-            {name: 'Alexander', surname: 'Onufrik'},
-            {name: 'Julia', surname: 'Yurchenko'},
-            {name: 'Sergey', surname: 'Potapov'},
-            {name: 'Andrey', surname: 'Marchukevich'},
-            {name: 'Sergey', surname: 'Shylobod'},
-            {name: 'Vadim', surname: 'Kalechits'},
-            {name: 'Andrey', surname: 'Tolkach'},
-            {name: 'Artem', surname: 'Prokopovich'},
-            {name: 'Nikita', surname: 'Lipskiy'}
-        ]
+
+    reRender () {
+        console.log("Success");
+    },
+
+    addPost() {
+        let newPost = {
+            id: 6,
+            text: this._state.profilePage.newPostData,
+            likes: 0
+        }
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostData = "";
+        this._reRender(this._state)
+    },
+
+    updatePost (updateText) {
+        this._state.profilePage.newPostData = updateText;
+        this._reRender(this._state);
+    },
+
+    subscribe (observer) {
+        this._reRender = observer;
     }
-}
 
-export const addPost = () => {
-
-    let newPost = {
-        id: 6,
-        text: state.profilePage.newPostData,
-        likes: 0
-    }
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostData = "";
-    reRender(state)
-}
-
-
-
-export const updatePost = (updateText) => {
-    state.profilePage.newPostData = updateText;
-    reRender(state);
-}
-
-export const subscribe = (observer) => {
-    reRender = observer;
 }
 
 
 
 
-
-export default state;
+export default store;
