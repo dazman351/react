c6767de8706540b9fe9f4674716b503164dc036c
Anton <anton124990qw@mail.ru>
2022-06-01 21:17:07 +0300

    install library axios

--- src/Components/Allusers/AllUsers.jsx
+++ src/Components/Allusers/AllUsers.jsx
@@ -1,18 +1,13 @@
 import './../../App.scss';
+import * as axios from 'axios';
 
 
 const AllUsers = (props) => {
     if (props.usersData.length === 0) {
-        props.setUsers([
-            {id: 1, name: "Sergey", surname: "Misevich", followed: true},
-            {id: 2, name: "Alexandr", surname: "Onufrik", followed: false},
-            {id: 3, name: "Igor", surname: "Jvirblya", followed: true},
-            {id: 4, name: "Vadim", surname: "Kalechits", followed: true},
-            {id: 5, name: "Yana", surname: "Prusevich", followed: false},
-            {id: 6, name: "Julia", surname: "Yurchenko", followed: true},
-            {id: 7, name: "Gena", surname: "Tolochko", followed: false},
-            {id: 8, name: "Anjela", surname:"Turevich", followed: false}
-        ])
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        })
     }
     
    
@@ -22,7 +17,6 @@ const AllUsers = (props) => {
                 props.usersData.map(u => 
                 <div key={u.id}>
                     <div>{u.name}</div>
-                    <div>{u.surname}</div>
                     <div>
                         {u.followed 
                         ? <button onClick={ () => {props.unfollow(u.id)}}>Unfollow</button>
