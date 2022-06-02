c6c14fbe16a6d59fd73777e5479dbd6c82817e3c
Anton <anton124990qw@mail.ru>
2022-06-02 20:19:32 +0300

    add class component

--- src/Components/Allusers/AllUsers.jsx
+++ src/Components/Allusers/AllUsers.jsx
@@ -1,34 +1,67 @@
 import './../../App.scss';
 import * as axios from 'axios';
+import React from 'react';
 
 
-const AllUsers = (props) => {
-    if (props.usersData.length === 0) {
+class AllUsers extends React.Component {
+    constructor(props) {
+        super(props)
 
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
+            this.props.setUsers(response.data.items);
         })
     }
     
-   
-    return (
+    render () {
+        return (
+        
         <div className="users__page">
             {
-                props.usersData.map(u => 
+                this.props.usersData.map(u => 
                 <div key={u.id}>
                     <div>{u.name}</div>
                     <div>
                         {u.followed 
-                        ? <button onClick={ () => {props.unfollow(u.id)}}>Unfollow</button>
-                        : <button onClick={ () => {props.follow(u.id)}}>Follow</button> 
+                        ? <button onClick={ () => {this.props.unfollow(u.id)}}>Unfollow</button>
+                        : <button onClick={ () => {this.props.follow(u.id)}}>Follow</button> 
                         }
                     </div>
                 </div>
                 )
             }
         </div>
-    );
+        );
+    }
 }
+
+
+// const AllUsers = (props) => {
+//     if (props.usersData.length === 0) {
+
+//         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+//             props.setUsers(response.data.items);
+//         })
+//     }
+    
+   
+//     return (
+//         <div className="users__page">
+//             {
+//                 props.usersData.map(u => 
+//                 <div key={u.id}>
+//                     <div>{u.name}</div>
+//                     <div>
+//                         {u.followed 
+//                         ? <button onClick={ () => {props.unfollow(u.id)}}>Unfollow</button>
+//                         : <button onClick={ () => {props.follow(u.id)}}>Follow</button> 
+//                         }
+//                     </div>
+//                 </div>
+//                 )
+//             }
+//         </div>
+//     );
+// }
 
 
 export default AllUsers;
