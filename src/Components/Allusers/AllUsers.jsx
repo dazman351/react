52f360ccc5b99b69aa7784818cfb2eacc8893c7e
Anton <anton124990qw@mail.ru>
2022-06-16 22:28:14 +0300

    add componentDidMount

--- src/Components/Allusers/AllUsers.jsx
+++ src/Components/Allusers/AllUsers.jsx
@@ -4,9 +4,8 @@ import React from 'react';
 
 
 class AllUsers extends React.Component {
-    constructor(props) {
-        super(props)
-
+    
+    componentDidMount () {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items);
         })
